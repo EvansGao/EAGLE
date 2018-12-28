1b5ae8a4f7889c30058a7b2d658128548922daa4
Tianshun Gao <tgao7@jhmi.edu>
2018-12-28 02:55:19 -0500

    Add files via upload

---
+++ enhancerinfo/getEnhancer.pl
@@ -0,0 +1,24 @@
+use List::MoreUtils qw(uniq);
+use List::Util qw(min max sum);
+$enhfile=$ARGV[0];
+mkdir("temp/enhancer");
+system("bedtools intersect -a ./enhancerinfo/allenhs.bed -b $enhfile -wa -wb>./temp/enhancer/cellenhspre.bed");
+	@tempenhs=();
+	%hashtempenh=();
+	open CELLENH,"./temp/enhancer/cellenhspre.bed";
+	while(<CELLENH>){
+	chomp($_);
+	@temp=split/\t/,$_;
+	if(!exists $hashtempenh{$temp[0]."\t".$temp[1]."\t".$temp[2]}){
+	$hashtempenh{$temp[0]."\t".$temp[1]."\t".$temp[2]}=$temp[7];
+	push @tempenhs,$temp[0]."\t".$temp[1]."\t".$temp[2];
+	}elsif(exists $hashtempenh{$temp[0]."\t".$temp[1]."\t".$temp[2]} && $hashtempenh{$temp[0]."\t".$temp[1]."\t".$temp[2]}<$temp[7]){
+	$hashtempenh{$temp[0]."\t".$temp[1]."\t".$temp[2]}=$temp[7];
+	}
+	}
+	close CELLENH;
+	open CELLSTANDARD,">./temp/enhancer/cellenhs.bed";
+	foreach $enh (@tempenhs){
+	print CELLSTANDARD $enh."\t".$hashtempenh{$enh}."\n";
+	}
+	close CELLSTANDARD;
